be8bc801261d83da3640e0f3b88badca75d7993a
Gabrielly Barros <arielybarros014@gmail.com>
2026-09-04 02:02:49 -0300

    Add GestaoContaBancaria class for account management

--- 2ºPeriodo/programacao-orientada-ao-objeto/conta/GestaoContaBancaria.java
+++ 2ºPeriodo/programacao-orientada-ao-objeto/conta/GestaoContaBancaria.java
@@ -1,1 +1,10 @@
+package conta;
 
+public class GestaoContaBancaria {
+    ContaBancaria Conta;
+
+    public void inicializarSaldo(double valorInicial){
+        Conta.saldo = valorInicial;
+    }
+        
+}
