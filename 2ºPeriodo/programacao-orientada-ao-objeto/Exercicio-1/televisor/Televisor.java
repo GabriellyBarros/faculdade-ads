5a3d3d934be6053c3dff3bb1b9a5acacdd48db30
Gabrielly Barros <arielybarros014@gmail.com>
2026-09-04 22:58:19 -0300

    Implement Televisor class with basic methods

--- 2ºPeriodo/programacao-orientada-ao-objeto/Exercicio-1/televisor/Televisor.java
+++ 2ºPeriodo/programacao-orientada-ao-objeto/Exercicio-1/televisor/Televisor.java
@@ -1,1 +1,28 @@
+package televisor;
+
+public class Televisor {
+    //Declaração das variáveis
+    public String marca;
+    public int polegadas, volume, canalAtual;
+    public boolean ligada;
+
+    public Televisor(String marca, int polegadas, int volume, int canalAtual, boolean ligada){
+        this.marca = marca;
+        this.polegadas = polegadas;
+        this.volume = volume;
+        this.canalAtual = canalAtual;
+        this.ligada = ligada;
+    }
+
+    //Método1 : ligar() - muda a variável 'ligada' para true
+    public void ligar(){
+        this.ligada = true;
+    }
+
+    //Método2 : trocarCanal(novoCanal)-atualiza 'canalAtual'
+    public void trocarCanal(int novoCanal){
+        this.canalAtual = novoCanal;
+    }
+    
+}
 
